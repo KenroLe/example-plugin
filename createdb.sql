8057be04034151b15bc3a7c31deca518a35b2b88
Kenro <kenro2002@gmail.com>
2023-07-07 18:54:12 +0000

    test

--- createdb.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE todo_list(id INTEGER PRIMARY KEY AUTOINCREMENT, description TEXT NOT NULL, completed TEXT DEFAULT 'No');
-INSERT INTO todo_list(description) VALUES('Test connecting to SQLite via Lua.');
